5f53d5a2bddec5349fdad0e09704136a45425345
Gokulakannan23 <gokulakannanezhil@gmail.com>
2024-07-09 14:44:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('off').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQujHRWelLcpY5psZ0iGq91hZenSc_w91M_cg&s"
+});
+document.getElementById('on').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROwf92MScFErTN87DlWlH1az39AjUXs8eOSA&s"
+});
